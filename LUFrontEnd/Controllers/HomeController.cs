e5b3bae9260b8780739ec6642db20f50f3b662c6
jmahoney2261 <jmahoney2261@yahoo.com>
2018-11-14 16:40:52 -0500

    First commit

---
+++ LUFrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LUFrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewBag.DegreeType = new List<SelectListItem>(){
+                new SelectListItem() { Text = "Select", Value = "Select" },
+                new SelectListItem() { Text = "Lorem", Value = "Lorem" },
+                new SelectListItem() { Text = "Ipsum", Value = "Ipsum" },
+                new SelectListItem() { Text = "Dolor", Value = "Dolor" },
+                new SelectListItem() { Text = "Sit", Value = "Sit" },
+                new SelectListItem() { Text = "Amet", Value = "Amet" }
+            };
+            ViewBag.StudyType = new List<SelectListItem>(){
+                new SelectListItem() { Text = "Select a degree type", Value = "Select a degree type" },
+                new SelectListItem() { Text = "Lorem", Value = "Lorem" },
+                new SelectListItem() { Text = "Ipsum", Value = "Ipsum" },
+                new SelectListItem() { Text = "Dolor", Value = "Dolor" },
+                new SelectListItem() { Text = "Sit", Value = "Sit" },
+                new SelectListItem() { Text = "Amet", Value = "Amet" }
+            };
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LUFrontEnd/Models/LUModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUFrontEnd.Models
+{
+    public class LUModel
+    {
+
+        public string DegreeType { get; set; }
+        public string StudyType { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+
+    }
+}
